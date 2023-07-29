f4cb4e7f375c65756477e27d76f1c7495d8e9cdb
nilang2003 <110675577+nilang2003@users.noreply.github.com>
2023-07-29 15:58:04 +0530

    Create practical1.js

---
+++ practical1.js
@@ -0,0 +1,21 @@
+// Create a program that declares and initializes variables of different data types 
+// (string, number, boolean) and displays their values. Write a function that takes 
+// two numbers as parameters and returns their sum.
+
+// this function is for addition of 2 numbers taking parameters
+function add(i, j) {
+    return i + j;
+}
+
+var str = "Nilang"; //this is string
+var num = 22;  //this is number
+var is_true = true;  //this is boolean
+
+//print the above values
+console.log(str)
+console.log(num)
+console.log(is_true)
+
+var a = 20;
+var b = 30;
+console.log(add(a, b));//calling function in print statement
